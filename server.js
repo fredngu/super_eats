73c2704a5a72e677afc10cdc1ce40ea38d792628
Frederik Nguyen <frederik.nguyen@outlook.com>
2023-07-24 09:46:14 -0400

    Add HTML and CSS for order page

--- server.js
+++ server.js
@@ -51,3 +51,7 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
+
+app.get('/restaurants', (req, res) => {
+  res.render(`restaurant`);
+});
