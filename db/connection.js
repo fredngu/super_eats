e13760d7077e758393a678fb8ac4cd48ba944394
Glauber Marques <glauberfmarques@gmail.com>
2023-07-24 17:10:40 -0600

    Prepared connection file to query directly into database

--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,20 @@
 // PG database client/connection setup
+// PG database client/connection setup
 const { Pool } = require('pg');
 
 const dbParams = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 5432,
+  user: process.env.DB_USER || 'labber',
+  password: process.env.DB_PASS || 'labber',
+  database: process.env.DB_NAME || 'supereats'
 };
 
 const db = new Pool(dbParams);
 
-db.connect();
+db.connect()
+  .then(() => console.log('Connected to the database'))
+  .catch(err => console.error('Error connecting to the database', err));
 
 module.exports = db;
+
