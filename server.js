1b77ee808401c00c7e161649fa19082e44be4ffe
Pabs <paul.mca.ca@gmail.com>
2023-07-25 21:55:33 +0000

    Add git placeholders

--- server.js
+++ server.js
@@ -26,6 +26,10 @@ app.use(
 );
 app.use(express.static('public'));
 
+//cookie middleware
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const userApiRoutes = require('./routes/users-api');
@@ -44,11 +48,57 @@ app.use('/users', usersRoutes);
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
 
+//redirect from empty directory to restaurants directory
+app.get("/", (req, res) => {
+  console.log("Root Directory! You will be redirected");
+  res.redirect('/restaurants')
+});
+
+//Homepage for supereats
+app.get('/restaurants', (req, res) => {
+  res.render('index');
+});
+
+//Move to Luigi's restaurant TODO - update to restaurants/01
+app.get('/restaurant', (req, res) => {
+  res.render('restaurant');
+});
+
+//COOKIE WIP
+app.get('/customers/signin/:id', (req, res) => {
+  res.render('customer_signin');
+});
+// app.post('/customers/signin/', (req, res) => {
+//   res.cookie('cookieName', '1', { expires: new Date(Date.now() + 900000), httpOnly: true })
+//   console.log(req.cookies.cookieName)}
+// )
+
+app.get("/restaurants/restaurant/orders", function(req,res){
+  res.render('restaurant_all_orders');
+});
+
+app.get("/restaurants/orders/:order_id", function(req,res){
+  console.log(`Fetching order details for Order #${req.params.order_id}`)
+  res.render('restaurant_order');
+});
+//TODO add post details
+//Update order by restaurant - PATCH
+//TODO convert to PATCH
+// app.post("/restaurants/orders/:order_id", function(req,res){
+//   if (!req.session.restaurant_id) {
+//     res.status(400).send("User must be logged in to create an order");
+//   } else {
+//     console.log(req.session.restaurant_id); // Log the POST request body to the console
+//     // TODO let restaurant_id = ??? & send order info
+//     res.redirect(`/restaurants/orders/${order_id}`);
+//   }});
+
+
+app.get("/orders/:order_id", function(req,res){
+  console.log(`Fetching order details for Order #${req.params.order_id}`)
+  res.render('client_order');
+});
